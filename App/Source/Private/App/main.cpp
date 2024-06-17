5a750093b8e484a0a8417f8f11dc4cc6ca192cc7
RedYggdrasil <dev.redyggdrasil@gmail.com>
2024-06-17 19:22:37 +0200

    Adding Tracy logging into solution

--- App/Source/Private/App/main.cpp
+++ App/Source/Private/App/main.cpp
@@ -5,29 +5,42 @@
 
 #include <iostream>
 
+
+#include "Tracy.hpp"
+#include "TracyC.h"
+
 using namespace AppNmsp;
 using namespace DirectX;
 int main(int argc, char** argv) 
 {
+	ZoneScopedN("Application");
+
 	Picture resultBuffer(XMINT2(256, 256), TEXT("Result.ppm"));
 
-	XMINT2 size = resultBuffer.GetSize();
-	XMFLOAT2 sizef = XMFLOAT2( (float)size.x, (float)size.y);
-
-	for (int32_t x = 0; x < size.x; ++x)
 	{
-		float xf = (float)x;
-		for (int32_t y = 0; y < size.y; ++y)
+		ZoneScopedN("Draw");
+		XMINT2 size = resultBuffer.GetSize();
+		XMFLOAT2 sizef = XMFLOAT2((float)size.x, (float)size.y);
+
+		for (int32_t x = 0; x < size.x; ++x)
 		{
-			resultBuffer[{x, y}].Color = Color
+			float xf = (float)x;
+			for (int32_t y = 0; y < size.y; ++y)
 			{
-				.R = (uint8_t)( (	    xf / sizef.x) * 255.f ),
-				.G = (uint8_t)( ( (float)y / sizef.y) * 255.f ),
-				.B = 127ui8
-			};
+				resultBuffer[{x, y}].Color = Color
+				{
+					.R = (uint8_t)((xf / sizef.x) * 255.f),
+					.G = (uint8_t)(((float)y / sizef.y) * 255.f),
+					.B = 127ui8
+				};
+			}
 		}
 	}
-	resultBuffer.WriteToDisk(true);
+
+	{
+		ZoneScopedN("Write to disk");
+		resultBuffer.WriteToDisk(true);
+	}
 	char r;
 	std::cin >> r;
 
