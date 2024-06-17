2a641df62fd6d13b1ec74d3d3f0d123fff20dbb5
RedYggdrasil <dev.redyggdrasil@gmail.com>
2024-06-17 02:22:14 +0200

    Empty project

---
+++ App/Source/Private/App/main.cpp
@@ -0,0 +1,16 @@
+
+#include "App/pch.h"
+#include "Gen_App/Config/AppConfig.h"
+
+#include <iostream>
+
+
+int main(int argc, char** argv) 
+{
+	std::cout << "Hello world !" << std::endl;
+
+	char r;
+	std::cin >> r;
+
+	return 0;
+}
