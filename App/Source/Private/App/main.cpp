0f472d8a2d8bad016df3b47b19af7f99014f70a3
RedYggdrasil <dev.redyggdrasil@gmail.com>
2024-06-18 04:18:35 +0200

    Simple coloring base on ray direction

--- App/Source/Private/App/main.cpp
+++ App/Source/Private/App/main.cpp
@@ -1,8 +1,10 @@
 #include "App/pch.h"
 #include "Gen_App/Config/AppConfig.h"
-#include "App/Tools/RLog.h"
 
+#include "App/Maths/Camera.h"
+#include "App/Maths/Ray.h"
 #include "App/SystemElement/Picture.h"
+#include "App/Tools/RLog.h"
 
 #include <iostream>
 
@@ -12,33 +14,60 @@
 
 using namespace AppNmsp;
 using namespace DirectX;
+
+inline XMFLOAT3 XM_CALLCONV RayColor(const RayVEC* InPlRay, const int32_t InRow) {
+	XMFLOAT3 result = XMFLOAT3(0.f, 0.f, 0.f);
+	XMVECTOR rayNorm = XMVector3Normalize(InPlRay->Direction);
+	
+	float a = 0.5f * (XMVectorGetZ(rayNorm) + 1.f);
+
+	static const XMVECTOR ColA{ 1.f, 1.f, 1.f, 0.f };
+	static const XMVECTOR ColB{ 0.5f, 0.7f, 1.f, 1.f };
+	XMStoreFloat3(&result, ((1.f - a) * ColA) + (a * ColB));
+	return result;
+}
+
 int main(int argc, char** argv) 
 {
 	ZoneScopedN("Application");
 
-	Picture resultBuffer(XMINT2(256, 256), TEXT("Result.ppm"));
+
+	Camera cameraData = Camera::FromSelectedCameraData(16.f / 9.f, 1.f, XMFLOAT3(0.f, 0.f, 0.f), 400, 2.f);
+	CameraVEC lCameraData;
+	XMLoadCamera(&lCameraData, &cameraData);
+
+
+	Picture resultBuffer(cameraData.ImageSize, TEXT("Result.ppm"));
+
+
 
 	{
 		ZoneScopedN("Draw");
 		XMINT2 size = resultBuffer.GetSize();
 		XMFLOAT2 sizef = XMFLOAT2((float)size.x, (float)size.y);
-		for (int32_t x = 0; x < size.x; ++x)
+
+		RLog::Log(LOG_DISPLAY, TEXT("Pixel00Pos = '{}'"), _tostring(cameraData.Pixel00Pos));
+		for (int32_t y = 0; y < size.y; ++y)
 		{
-			static int32_t lastLogs = x / 10;
-			if ((x / 10) > lastLogs)
+			static int32_t lastLogs = y / 10;
+			if ((y / 10) > lastLogs)
 			{
-				RLog::Log(LOG_DISPLAY, TEXT("In Row x = '{}'"), x);
-				lastLogs = x / 10;
+				RLog::Log(LOG_DISPLAY, TEXT("In Row y = '{}'"), y);
+				lastLogs = y / 10;
 			}
-			float xf = (float)x;
-			for (int32_t y = 0; y < size.y; ++y)
+			float yf = (float)y;
+			for (int32_t x = 0; x < size.x; ++x)
 			{
-				resultBuffer[{x, y}] = XMFLOAT3
+				float xf = (float)x;
+				XMVECTOR lPixelCenter = lCameraData.Pixel00Pos + (xf * lCameraData.PixelDeltaU) + (yf * lCameraData.PixelDeltaV);
+				XMVECTOR lRayDir = lPixelCenter - lCameraData.CameraCenter;
+				RayVEC lRay
 				{
-					(xf			/ sizef.x),
-					((float)y	/ sizef.y),
-					0.0f
+					.Origin = lCameraData.CameraCenter,
+					.Direction = lRayDir
 				};
+
+				resultBuffer[{x, y}] = RayColor(&lRay, yf);
 			}
 		}
 	}
