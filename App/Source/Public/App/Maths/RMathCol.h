b112f85dc93c3f6021b8d806d4063a8edb2f8bae
RedYggdrasil <dev.redyggdrasil@gmail.com>
2024-06-26 21:48:02 +0200

    SphereCollisionOnRay simplification

--- App/Source/Public/App/Maths/RMathCol.h
+++ App/Source/Public/App/Maths/RMathCol.h
@@ -15,17 +15,20 @@ namespace AppNmsp
         inline static float XM_CALLCONV SphereCollisionOnRay(const RayVECAnyNrm* InRayVec, DirectX::FXMVECTOR InSphereCenter, const float InRadius)
         {
             DirectX::XMVECTOR RayOriginToSphereCenter = DirectX::XMVectorSubtract(InSphereCenter, InRayVec->Origin);
-            const float a = DirectX::XMVectorGetX(DirectX::XMVector3Dot(InRayVec->Direction, InRayVec->Direction));
-            const float b = -2.f * DirectX::XMVectorGetX(DirectX::XMVector3Dot(InRayVec->Direction, RayOriginToSphereCenter));
-            const float c = DirectX::XMVectorGetX(DirectX::XMVector3Dot(RayOriginToSphereCenter, RayOriginToSphereCenter)) - (InRadius * InRadius);
-            const float discriminant = b * b - 4.f * a * c;
+
+            const float a = DirectX::XMVectorGetX(DirectX::XMVector3LengthSq(InRayVec->Direction));
+
+            const float h = DirectX::XMVectorGetX(DirectX::XMVector3Dot(InRayVec->Direction, RayOriginToSphereCenter));
+            const float c = DirectX::XMVectorGetX(DirectX::XMVector3LengthSq(RayOriginToSphereCenter)) - (InRadius * InRadius);
+            const float discriminant = h * h - a * c;
             if (discriminant < 0) {
                 return -1.0f;
             }
             else {
-                return (-b - sqrtf(discriminant)) / (2.f * a);
+                return (h - sqrtf(discriminant)) / a;
             }
         }
+
 		inline static bool XM_CALLCONV RaySphereCollide(const RayVECAnyNrm* InRayVec, DirectX::FXMVECTOR InSphereCenter, const float InRadius)
 		{
             return SphereCollisionOnRay(InRayVec, InSphereCenter, InRadius) >= 0.f;
